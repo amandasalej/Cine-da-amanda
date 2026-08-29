fc8005862e3d029bde0c9f4a03163b531d642bac
amandasalej <asalejtorres@usp.br>
2026-08-28 21:48:44 -0300

    Atualizar o streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,32 @@ st.markdown("""
 """)
 
 st.image(
-    "https://images.unsplash.com/photo-1512820790803-83ca734da794?w=800&q=80", 
+    "https://unsplash.com/pt-br/fotografias/um-vaso-de-flores-sentado-em-cima-de-uma-pilha-de-livros-CERxldZpJIs", 
     caption="Meu mundo em páginas! 🌸✨"
 )
+
+st.markdown("### 📝 **Cadastrar Nova Leitura**")
+st.markdown("Preencha os detalhes abaixo para guardar mais um livro na sua estante. 💖")
+
+# Formulário
+with st.form("form_cadastro_livro"):
+    
+    titulo = st.text_input("Título do Livro 📖", placeholder="Ex: Os Sete Maridos de Evelyn Hugo")
+    autor = st.text_input("Autor(a) ✍️", placeholder="Ex: Taylor Jenkins Reid")
+    
+    nota = st.number_input("Sua Nota (1 a 5) ⭐", min_value=1, max_value=5, step=1)
+    
+    data_termino = st.date_input("Quando você terminou de ler? 📅", format="DD/MM/YYYY")
+    
+    cor_capa = st.color_picker("Qual a cor da capa? 🎨")
+    
+    btn_salvar = st.form_submit_button("Guardar na Estante 🎀")
+    
+    if btn_salvar:
+        if not titulo:
+            st.warning("Eii, você esqueceu de preencher o título!!")
+        else:
+            st.success(f"Eba! O livro **{titulo}** foi cadastrado com sucesso! 🎉")
+            st.balloons() # Uma animação fofa de balões!
 
 
