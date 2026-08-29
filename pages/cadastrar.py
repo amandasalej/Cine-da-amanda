99d165e02e1ac53fc5fa5942b1f8120c2e4ead06
amandasalej <asalejtorres@usp.br>
2026-08-28 23:22:15 -0300

    Atualizar o cadastrar.py

--- pages/cadastrar.py
+++ pages/cadastrar.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+if not firebase_admin._apps:
+    cred_dict = dict(st.secrets["firebase"])
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 st.markdown("### 📝 **Cadastrar Nova Leitura**")
 st.markdown("Preencha os detalhes abaixo para guardar mais um livro na sua estante. 💖")
@@ -7,14 +16,26 @@ with st.form("form_cadastro_livro"):
     titulo = st.text_input("Título do Livro 📖", placeholder="Ex: Os Sete Maridos de Evelyn Hugo")
     autor = st.text_input("Autor(a) ✍️", placeholder="Ex: Taylor Jenkins Reid")
     nota = st.number_input("Sua Nota (1 a 5) ⭐", min_value=1, max_value=5, step=1)
-    data_termino = st.date_input("Quando você terminou de ler? 📅", format="DD/MM/YYYY")
+    data_termino = st.date_input("Quando terminou de ler? 📅", format="DD/MM/YYYY")
     cor_capa = st.color_picker("Qual a cor principal da capa? 🎨")
     
     btn_salvar = st.form_submit_button("Guardar na Estante 🎀")
     
     if btn_salvar:
         if not titulo:
-            st.warning("Poxa, você esqueceu de preencher o título! 🥺")
+            st.warning("Oh, esqueceu-se de preencher o título! 🥺")
         else:
-            st.success(f"Eba! O livro **{titulo}** foi cadastrado com sucesso! 🎉")
+            
+            novo_livro = {
+                "titulo": titulo,
+                "autor": autor,
+                "nota": nota,
+                "data_termino": str(data_termino), 
+        		"cor_capa": cor_capa
+            }
+            
+            
+            db.collection("livros").add(novo_livro)
+            
+            st.success(f"BOA! O livro **{titulo}** foi guardado no banco de dados com sucesso! 🎉")
             st.balloons()
